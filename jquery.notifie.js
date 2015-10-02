27952653caa1aa15f0ed3a07fb7d402f9d01db89
marcinzarycki <marcin.zarycki@kineticdata.com>
2015-10-02 09:15:28 -0500

    Message can now be hmtl. Added toggle option. Fixed bugs.
    - The message can now contain html.
    - Added toggle option, which when set to true, will only close any
    existing notifications, and if no existing notifications then will open
    new notification.
    - Changed exitEvents option to only apply if a single notification will
    be opened via the .notfie call.
    - Added event.preventDefault to exit event so it won't trigger form
    submits.
    - Fixed issue where not all existing notifications were being cleared
    when using exit option.
    - Fixed bug where when calling .notifie on a set of elements,
    functionality didn't work properly.

--- jquery.notifie.js
+++ jquery.notifie.js
@@ -1,6 +1,6 @@
 /**
  * Notifie JS
- * Version 0.2.1
+ * Version 0.3
  * 
  * Library that extends jQuery to add a .notifie(options) function for displaying alerts and confirmations.
  * Requires jQuery, Bootstrap, FontAwesome
@@ -57,6 +57,7 @@
  * 
  * 	-exitEvents:	String -> Javascript event name(s)
  * 					Default: ''
+ * 					Applies only when 1 notification will be opened (ie: calling element or its anchor translates toa  single element, not a list of elements).
  * 					Events that will get added to calling element that will close the notifications.
  * 					You may pass multiple events by separating them by spaces.
  * 					Examples: 'click' or 'focus' or 'click focus'
@@ -86,9 +87,13 @@
  * 					Default: true
  * 					If true, the calling element will be disabled when the notification is shown, and enabled when the notification is closed.
  * 
+ * 	-toggle:		Boolean
+ * 					Default: false
+ * 					If true and if notification at the anchor level already exists, this .notifie() call will only close that notification.
+ * 
  * 	-exit:			Boolean
  * 					Default: false
- * 					If true, this .notifie() call will only close the notification(s) at the anchor level
+ * 					If true, this .notifie() call will only close the notification(s) at the anchor level.
  * 
  * 	-recurseExit:	Boolean
  * 					Default: false
@@ -116,26 +121,27 @@ jQuery.fn.extend({
 			duration: 100,
 			expire: null,
 			disable: true,
+			toggle: false,
 			exit: false,
 			recurseExit: false
 		}, options || {});
 		
 		// Get owner of notification
 		var owner = self.closest(options.anchor).length ? self.closest(options.anchor) : self;
-			
+		
 		// Close existing notification(s)
-		if (options.exit){
+		if (options.exit || (owner.prevUntil(':not(div.notifie)').length > 0 && options.toggle)){
 			// If recurseExit is true, close all notifications under owner
 			if (options.recurseExit){
-				// Find notifications in siblings and close
-				owner.siblings('div.notifie').trigger('exit', false);
+				// Find notifications immediately before owner and close
+				owner.prevUntil(':not(div.notifie)').trigger('exit', false);
 				// Find all notifications in all descendants close
 				owner.find('div.notifie').trigger('exit', false);
 			}
 			// If recurseExit is false, close only children notifications under owner
 			else {
-				// Find sibling notifications and close
-				owner.siblings('div.notifie').trigger('exit', false);
+				// Find notifications immediately before owner and close
+				owner.prevUntil(':not(div.notifie)').trigger('exit', false);
 			}
 		}
 		// Show new notification
@@ -200,20 +206,26 @@ jQuery.fn.extend({
 					// Remove the notification
 					$(this).remove();
 				});
+				// Prevent default on this event, so it doesn't trigger form submits
+				if(e.preventDefault) e.preventDefault();
+				else e.returnValue = false;
 			});
 			
-			// Add one time events to self for exiting the notifications
-			self.one(options.exitEvents, function(){
-				notification.trigger('exit', false);
-			});
+			// If only 1 owner, therefore only 1 notification 
+			if (owner.length <= 1){
+				// Add one time events to self for exiting the notifications
+				self.one(options.exitEvents, function(e){
+					notification.trigger('exit', false);
+				});
+			}
 
 			// Build notification close button and attach exit event function on click
 			$("<button>").addClass('fa fa-times close').on('click', function(e){
 				// Close popup and call appropriate onConfirm/onReject function (which is handled by the exit event)
-				notification.trigger('exit', false);
+				$(this).closest('.notifie').trigger('exit', false);
 			}).appendTo(notification);
 			// Build notification message
-			$("<div>").addClass("notifie-message").text(options.message).appendTo(notification);
+			$("<div>").addClass("notifie-message").html(options.message).appendTo(notification);
 			// If confirm, build buttons
 			if (isConfirm){
 				// Build button group
@@ -227,7 +239,7 @@ jQuery.fn.extend({
 				// Add click event to button
 				rejectBtn.on('click', function(e){
 					// Close popup and call appropriate onConfirm/onReject function (which is handled by the exit event)
-					notification.trigger('exit', false);
+					$(this).closest('.notifie').trigger('exit', false);
 				});
 				
 				// Create button
@@ -238,17 +250,15 @@ jQuery.fn.extend({
 				// Add click event to button
 				confirmBtn.on('click', function(e){
 					// Close popup and call onConfirm function (which is handled by the exit event)
-					notification.trigger('exit', true);
+					$(this).closest('.notifie').trigger('exit', true);
 				});
 			}
 			
 			// Clear any existing notifications if required
 			if (options.clearExisting) owner.prevUntil(':not(div.notifie)').trigger('exit', false);
-			// Add as sibling immediately before owner
-			owner.before(notification);
 			
-			// Show notification
-			notification.slideDown(options.duration, function(){
+			// Add as sibling immediately before owner and show notification
+			notification.insertBefore(owner).slideDown(options.duration, function(){
 				// Call onShow function
 				options.onShow.call(self);
 				// Disable self if necessary
@@ -256,8 +266,9 @@ jQuery.fn.extend({
 					self.prop('disabled', true);
 				}
 				if (isAlert && options.expire){
+					var nSelf = $(this);
 					setTimeout(function() {
-						notification.trigger('exit', false);
+						nSelf.trigger('exit', false);
 				    }, options.expire);
 				}
 			});
@@ -271,6 +282,13 @@ jQuery.fn.extend({
 /**
  * Change Log
  * 
+ * v0.3 2015-10-02
+ *  - The message can now contain html.
+ * 	- Added toggle option, which when set to true, will only close any existing notifications, and if no existing notifications then will open new notification. 
+ * 	- Changed exitEvents option to only apply if a single notification will be opened via the .notfie call.
+ * 	- Added event.preventDefault to exit event so it won't trigger form submits.
+ * 	- Fixed issue where not all existing notifications were being cleared when using exit option.
+ * 	- Fixed bug where when calling .notifie on a set of elements, functionality didn't work properly.
  * v0.2.1 2015-09-10
  * 	- Fixed some styling issues and clearExisting issue where not all existing notifications were being cleared.
  * v0.2 2015-09-09
